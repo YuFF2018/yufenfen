c6c93ea2bee34efaeac9e7edae7e5b4cad39078d
YuFF2018 <yufenfen@aidigger.com>
2018-08-30 17:28:00 +0800

    add new exercise-2.20.3chessBoard

---
+++ Exercises/2.20.3/index.js
@@ -0,0 +1,13 @@
+var str = " ";
+var size = 8;
+
+for (var y=0; y<size; y++) {
+    for (var x=0; x<size; x++){
+        if ((x+y) % 2 === 0)
+            str += "#";
+        else
+        str += " ";
+    }
+    str += "\n";
+}
+console.log(str);
